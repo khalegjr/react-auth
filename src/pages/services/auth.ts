24b19f22a04867958ba0dfe7f351c6fb948dde4a
Khaled <khalegjr@gmail.com>
2022-12-28 00:35:09 -0300

    Cria um serviço que simula uma chamada de autenticação no back

---
+++ src/pages/services/auth.ts
@@ -0,0 +1,21 @@
+import { v4 as uuid } from "uuid";
+type signInRequestData = {
+  email: string;
+  password: string;
+};
+
+const delay = (amount = 750) =>
+  new Promise((resolve) => setTimeout(resolve, amount));
+
+export async function signInRequest(data: signInRequestData) {
+  await delay();
+
+  return {
+    token: uuid(),
+    user: {
+      name: "Júnior Khaled",
+      email: "jk@email.com",
+      avatar_url: "https://github.com/khalegjr.png",
+    },
+  };
+}
