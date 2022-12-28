27f99e9b63cb3b76c6f8e02dafd14571113ddeea
Khaled <khalegjr@gmail.com>
2022-12-28 04:03:23 -0300

    Cria um interceptor para verificar se o token foi enviado

--- src/pages/services/api.ts
+++ src/pages/services/api.ts
@@ -7,6 +7,11 @@ export const api = axios.create({
   baseURL: "http://localhost:3333",
 });
 
+api.interceptors.request.use((config) => {
+  console.log(config);
+  return config;
+});
+
 if (token) {
   api.defaults.headers["Authorization"] = `Bearer ${token}`;
 }
